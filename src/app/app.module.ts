daf55b361126878c287286776c17a328a18dbf1a
kocherlaharsha <kocherlaharsha0426@gmail.com>
2018-09-14 17:50:49 -0500

    changes should see later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { AlertComponent } from './alert/alert.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    AlertComponent,
+    HomeComponent,
+    RegistrationFormComponent,
   ],
   imports: [
     BrowserModule
